57995d8373783de2321316301237b2459818fce9
Vlad <vladyabl15@gmail.com>
2024-10-12 12:33:35 +0200

    homework 01 solution

---
+++ hw01/t1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace t1
+{
+    internal class Program
+    {
+        private static void ListAADigits(int a, int b)
+        {
+            if (b < 130)
+                return;
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = a; i <= b; i++)
+            {
+                int decimalNumber = i;
+                int aCounter = 0;
+                while (decimalNumber > 0)
+                {
+                    int remainder = decimalNumber % 12;
+                    decimalNumber = decimalNumber / 12;
+                    switch (remainder)
+                    {
+                        case 10:
+                            stringBuilder.Insert(0, 'A');
+                            aCounter++;
+                            break;
+                        case 11:
+                            stringBuilder.Insert(0, 'B');
+                            break;
+                        default:
+                            stringBuilder.Insert(0, remainder);
+                            break;
+                    }
+                }
+
+                if (aCounter == 2)
+                    Console.WriteLine(stringBuilder.ToString());
+
+                stringBuilder.Clear();
+            }
+
+        }
+
+
+        private static void Solution()
+        {
+            Console.WriteLine("Enter two integers separeted by space");
+            string[] input = Console.ReadLine()!.Split(' ');
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            ListAADigits(a, b);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Solution();
+        }
+    }
+}
---
+++ hw01/t2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace t2
+{
+    internal class Program
+    {
+        private static string CreateISBN(string digits)
+        {
+            int checkSum = 0;
+            for (int i = 0; i < digits.Length; i++)
+            {
+                checkSum += (10 - i) * int.Parse(digits[i].ToString());
+            }
+
+
+            int remainder = checkSum % 11;
+            int checkDigit = 11 - remainder;
+            if (checkDigit == 10)
+                return digits + "X";
+            else
+                return digits + checkDigit;
+        }
+
+        private static void Solution()
+        {
+            string first9Digits = Console.ReadLine();
+            Console.WriteLine(CreateISBN(first9Digits));
+        }
+
+        static void Main(string[] args)
+        {
+            Solution();
+        }
+    }
+}
