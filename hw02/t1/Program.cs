e81883c993c4668eb25f1e526cdabd8a1ec24df1
Vlad <vladyabl15@gmail.com>
2024-10-26 12:06:27 +0200

    t1 fix

--- hw02/t1/Program.cs
+++ hw02/t1/Program.cs
@@ -2,34 +2,34 @@
 {
     class Point
     {
-        public int X { get => point[0]; set => point[0] = value; }
-        public int Y { get => point[1]; set => point[1] = value; }
-        public int Z { get => point[2]; set => point[2] = value; }
-        public  float Mass { get => mass; set => mass = value < 0 ? 0 : value; }
+        public int X { get => _point[0]; set => _point[0] = value; }
+        public int Y { get => _point[1]; set => _point[1] = value; }
+        public int Z { get => _point[2]; set => _point[2] = value; }
+        public  float Mass { get => _mass; set => _mass = value < 0 ? 0 : value; }
 
-        private int[] point;
-        private float mass;
+        private int[] _point;
+        private float _mass;
 
         public Point(int x, int y, int z, float mass = 0)
         {
-            point = new int[3];
-            point[0] = x;   
-            point[1] = y;
-            point[2] = z;
-            this.mass = mass;
+            _point = new int[3];
+            X = x;   
+            Y = y;
+            Z = z;
+            this._mass = mass;
         }
 
         public bool IsZero()
         {
-            return point[0] == 0 && point[1] == 0 && point[2] == 0;
+            return X == 0 && Y == 0 && Z == 0;
         }
 
         public double Distance(Point other)
         {
             return Math.Sqrt(
-                (point[0] - other.point[0]) * (point[0] - other.point[0]) +
-                (point[1] - other.point[1]) * (point[1] - other.point[1]) +
-                (point[2] - other.point[2]) * (point[2] - other.point[2])
+                (X - other.X) * (X - other.X) +
+                (Y - other.Y) * (Y - other.Y) +
+                (Z - other.Z) * (Z - other.Z)
             );
         }
     }
