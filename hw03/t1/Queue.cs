253d5a7e20904d3a0aad132b706a87b7c08324b1
Vlad <vladyabl15@gmail.com>
2024-10-28 20:01:04 +0100

    extensions class moved to separate file

--- hw03/t1/Queue.cs
+++ hw03/t1/Queue.cs
@@ -3,26 +3,6 @@
 namespace t1
 {
 
-    internal static class QueueExtensions
-    {
-        public static IQueue<T> Tail<T>(this IQueue<T> queue) where T : struct
-        {
-            int tailQueueSize = queue.Size() - 1;
-            Queue<T> tailQueue = new Queue<T>(tailQueueSize);
-            T head = queue.Dequeue();
-            queue.Enqueue(head);
-
-            for (int i = 0; i < tailQueueSize; i++)
-            {
-                T element = queue.Dequeue();
-                tailQueue.Enqueue(element);
-                queue.Enqueue(element);
-            }
-
-            return tailQueue;
-        }
-    }
-
     // array based implementation
     internal class Queue<T> : IQueue<T> where T : struct
     {
---
+++ hw03/t1/QueueExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace t1
+{
+    internal static class QueueExtensions
+    {
+        public static IQueue<T> Tail<T>(this IQueue<T> queue) where T : struct
+        {
+            int tailQueueSize = queue.Size() - 1;
+            Queue<T> tailQueue = new Queue<T>(tailQueueSize);
+            T head = queue.Dequeue();
+            queue.Enqueue(head);
+
+            for (int i = 0; i < tailQueueSize; i++)
+            {
+                T element = queue.Dequeue();
+                tailQueue.Enqueue(element);
+                queue.Enqueue(element);
+            }
+
+            return tailQueue;
+        }
+    }
+}
