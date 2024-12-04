41e32cc10df3f0a3be5c5e0169721aabb2b29848
Vlad <vladyabl15@gmail.com>
2024-12-04 13:30:37 +0100

    naming fix

--- HW05/t2/Repositories/XMLRepository.cs
+++ HW05/t2/Repositories/XMLRepository.cs
@@ -8,23 +8,23 @@ namespace t2.Serializers
 {
     internal class XMLRepository : IRepository<Catalog>
     {
-        private static readonly string repositoryDataPath = @".\PersistentData\catalog.xml";
-        private static readonly XmlSerializer Serializer = new XmlSerializer(typeof(CatalogSerializationModel));
+        private static readonly string _repositoryDataPath = @".\PersistentData\catalog.xml";
+        private static readonly XmlSerializer _serializer = new XmlSerializer(typeof(CatalogSerializationModel));
 
         public void Serialize(Catalog catalog)
         {
             CatalogSerializationModel catalogSM = new CatalogSerializationModel(catalog);
-            using (var writer = new StreamWriter(repositoryDataPath))
+            using (var writer = new StreamWriter(_repositoryDataPath))
             {
-                Serializer.Serialize(writer, catalogSM);
+                _serializer.Serialize(writer, catalogSM);
             }
         }
 
         public Catalog Deserialize()
         {
-            using (var reader = new StreamReader(repositoryDataPath))
+            using (var reader = new StreamReader(_repositoryDataPath))
             {
-                CatalogSerializationModel? catalogSM = Serializer.Deserialize(reader) as CatalogSerializationModel;
+                CatalogSerializationModel? catalogSM = _serializer.Deserialize(reader) as CatalogSerializationModel;
                 if (catalogSM == null)
                     throw new XmlException("Can't parse given xml file");
 
